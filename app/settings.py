1462d914d41eb0627e6b74167663eb6698ee502a
Leonardo Sobrinho <leonardosobrinho2003@hotmail.com>
2025-04-04 14:54:14 -0300

    Corrigindo config do JWT no  settings.py

--- app/settings.py
+++ app/settings.py
@@ -141,6 +141,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
